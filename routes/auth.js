c75acc804f3f6ee0bedbbecaf4f4b693b9828ac5
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-10-25 18:38:08 +0100

    User updates

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,6 @@ const router = express.Router();
 const AuthController =require ("../controllers/auth")
 
 router.route("/register").post(AuthController.register);
-router.route("/update").post(AuthController.updateUserByEmail);
-router.route("/getuser").get(verifyToken,AuthController.getUserByEmail);
 
 
 module.exports = router;
