d3b4e31327111e3bb052afbf399fc3cf1b2408eb
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-10-25 23:47:44 +0100

    User delete

--- models/User.js
+++ models/User.js
@@ -16,20 +16,12 @@ const UserSchema = new mongoose.Schema({
     type: String,
     enum: ["admin", "user", "guest"], 
     default: "user"
-  },
-  id: {
-    type: Number,
-    unique: true
   }
 });
 
 UserSchema.pre('save', async function (next) {
   try {
-    if (!this.id) {
-      const count = await mongoose.model('User').countDocuments();
-      this.id = count + 1;
-    }
-
+   
     if (this.isModified('password')) {
       const salt = await bcrypt.genSalt(10);
       this.password = await bcrypt.hash(this.password, salt);
