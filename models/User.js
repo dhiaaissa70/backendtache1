e78cf9c9870952ca8f04f65ac966e09a4269f4fd
Dhia aissa <dhia.aissa@esprit.tn>
2024-10-26 15:28:31 +0100

    login updated4

--- models/User.js
+++ models/User.js
@@ -19,19 +19,7 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-UserSchema.pre('save', async function (next) {
-  try {
-   
-    if (this.isModified('password')) {
-      const salt = await bcrypt.genSalt(10);
-      this.password = await bcrypt.hash(this.password, salt);
-    }
 
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
