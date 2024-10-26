ba7d627c894f0caa662300a0aba91d222c981adb
Dhia aissa <dhia.aissa@esprit.tn>
2024-10-26 14:15:05 +0100

    login updated

--- models/User.js
+++ models/User.js
@@ -14,8 +14,8 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["SuperAdmin", "Admin", "Partner","Assistant"], 
-    default: "user"
+    enum: ["SuperAdmin", "Admin", "Partner","Assistant","User"], 
+    default: "User"
   }
 });
 
