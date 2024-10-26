d248397896709ca758adee0b05623ef2609ca72a
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-10-26 23:17:17 +0100

    updates

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const AuthController =require ("../controllers/auth")
 
 router.route("/register").post(AuthController.register);
-router.route("/users_Role").post(AuthController.getUsersByRole); 
+router.route("/usersByRole").post(AuthController.getUsersByRole); 
 router.route("/getallusers").get(AuthController.getAllUsers); 
 router.route("/delete_user").delete(AuthController.deleteUserByUsername);
 router.route("/login").post(AuthController.login);
