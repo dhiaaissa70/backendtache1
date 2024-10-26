bb1fb2fd73da59429e57759ffba655dd82bd77e9
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-10-26 17:55:29 +0100

    Balance added

--- models/User.js
+++ models/User.js
@@ -15,11 +15,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     enum: ["SuperAdmin", "Admin", "Partner", "Assistant", "User"], 
     default: "User"
-  },
-  balance: {
-    type: Number,
-    default: 1000, 
-    min: 0
   }
 });
 
