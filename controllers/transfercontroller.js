6f15807c0b549bacb9b006fda4ab78cc19412998
Dhia aissa <dhia.aissa@esprit.tn>
2024-10-27 05:53:20 +0100

    transfer history6

--- controllers/transfercontroller.js
+++ controllers/transfercontroller.js
@@ -128,3 +128,16 @@ exports.getTransferHistory = async (req, res, next) => {
         return res.status(500).json({ message: "Error fetching transfer history." });
     }
 };
+
+exports.getUserInfo = async (req, res) => {
+    const { username } = req.params;
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        res.status(200).json({ success: true, user });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
+};
